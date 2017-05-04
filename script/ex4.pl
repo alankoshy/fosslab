c81a0d6a31334b7b93fe7976216cbdf366b1f4fb
alankoshy <alankoshy16@gmail.com>
2017-05-04 19:42:35 +0530

    Perl

---
+++ script/ex4.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+print "enter the number";
+$n=<>;
+$t=$n;
+$s=0;
+while($n>0){
+$r=$n%10;
+$i=$r*$r*$r;
+$s=$s+$i;
+$n=$n/10;
+}
+if($s==$t){
+print"Armstrong no";
+else{
+print"Not armstrong"
+}
+
